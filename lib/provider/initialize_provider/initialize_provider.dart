1e910892a05753aaaf56f49926e7140216511223
dogfoot <wownsdl12@naver.com>
2024-06-04 03:42:03 +0900

    baseURL

--- lib/provider/initialize_provider/initialize_provider.dart
+++ lib/provider/initialize_provider/initialize_provider.dart
@@ -1,5 +1,6 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:token_dio/token_dio.dart';
+import 'package:youtube_translation/config/config.dart';
 import 'package:youtube_translation/provider/screen_provider/screen_provider.dart';
 
 part 'initialize_provider.g.dart';
@@ -7,7 +8,7 @@ part 'initialize_provider.g.dart';
 @Riverpod()
 Future<bool> initialize(InitializeRef ref) async {
   await TokenDio.initialize(
-      baseURL: 'http://localhost:3003',
+      baseURL: Config.baseURL,
       getAccessTokenPath: '/user/get_access_token',
       getAccessTokenBodyBuilder: () async => {
             'accessToken': await ref.read(screenProvider).googleAccessToken
