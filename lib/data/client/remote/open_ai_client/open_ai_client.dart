07eb00103d611c5d627789030cae0de00aced6c2
dogfoot <wownsdl12@naver.com>
2024-07-07 23:03:24 +0900

    main

--- lib/data/client/remote/open_ai_client/open_ai_client.dart
+++ lib/data/client/remote/open_ai_client/open_ai_client.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'open_ai_client.g.dart';
-
+//
 @RestApi(baseUrl: OpenAiClient.baseUrl)
 abstract class OpenAiClient {
   factory OpenAiClient(Dio dio, {String baseUrl}) = _OpenAiClient;
